c45e2c0e8e633bcc2320e6d0551be0624bc76c81
TS22082 <ts22082@gmail.com>
2020-07-29 18:13:54 -0700

    add routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,14 +1,10 @@
 const router = require("express").Router();
-const {
-  getUser,
-  newUser,
-  deleteUser,
-} = require("../controllers/user-controller");
+const userController = require("../controllers/user-controller");
 
-router.get("/user", getUser);
-
-router.post("/user", newUser);
-
-router.delete("/user", deleteUser);
+router
+  .route("/user")
+  .get(userController.getUser)
+  .post(userController.newUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
