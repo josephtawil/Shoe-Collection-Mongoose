a671f504d3735bfe1047efbf60309d2d57c6f9ad
Joseph <jtawil2015@gmail.com>
2020-07-29 18:37:39 -0800

    another way of writing user routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const {getUser, addUser, deleteUser} = require('../controllers/user-controller');
-
-// get a single user or all users
-router.get("/user", getUser);
-
-//create a user 
-router.post("/user", addUser);
-router.delete("/delete", deleteUser);
-module.exports = router;
-=======
 const router = require("express").Router();
 const userController = require("../controllers/user-controller");
 
@@ -21,4 +8,3 @@ router
   .delete(userController.deleteUser);
 
 module.exports = router;
->>>>>>> c45e2c0e8e633bcc2320e6d0551be0624bc76c81
